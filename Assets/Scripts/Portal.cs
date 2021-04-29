8ac4dbce2de0cedb0da00d7ca7054a004512db06
Jay Wright <iamjwright159@gmail.com>
2021-04-29 02:36:29 -0400

    Fix portals to work with URP
    FIXME: You can't see opaque things on the other side of the portal for some reason

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace WrightWay.YellowVR
 {
@@ -13,12 +15,12 @@ namespace WrightWay.YellowVR
 		public Portal otherPortal;
 
 		/// <summary>
-		/// The <see cref="Camera"/> to render the outside of this <see cref="Portal"/> from.
+		/// The <see cref="Camera"/> to render onto the <see cref="renderTex"/>.
 		/// </summary>
 		public Camera renderCamera;
 
 		/// <summary>
-		/// The <see cref="MeshRenderer"/> containing the <see cref="Material"/> to be overriden with the <see cref="RenderTexture"/>.
+		/// The <see cref="MeshRenderer"/> containing the <see cref="Material"/> to be overriden with the <see cref="renderTex"/>.
 		/// </summary>
 		public MeshRenderer meshRenderer;
 
@@ -32,27 +34,38 @@ namespace WrightWay.YellowVR
 			meshRenderer.material.SetTexture("_MainTex", renderTex);
 		}
 
+		private void Start()
+		{
+			RenderPipelineManager.beginCameraRendering += RenderPortal;
+		}
+
+		private void OnDestroy()
+		{
+			RenderPipelineManager.beginCameraRendering -= RenderPortal;
+		}
+
 		/// <summary>
 		/// Renders the view on the other side of the portal from the perspective of a given <see cref="Camera"/>.
 		/// </summary>
-		/// <param name="camera">The <see cref="Camera"/> on (this? the other? TODO) side of the <see cref="Portal"/>.</param>
-		public void Render(Camera camera)
+		/// <param name="frameCamera">The <see cref="Camera"/> that is rendering the frame.</param>
+		public void RenderPortal(ScriptableRenderContext context, Camera frameCamera)
 		{
-			bool onFrontSide = transform.InverseTransformPoint(camera.transform.position).y >= 0;
+			bool onFrontSide = transform.InverseTransformPoint(frameCamera.transform.position).y >= 0;
 
 			// maincam_world -> get local to thisportal -> transform to local to otherportal (but all of this is backwards bc matrices) (not flipping bc convenicence of normals)
-			Matrix4x4 transformMatrix = otherPortal.transform.localToWorldMatrix * transform.worldToLocalMatrix * camera.transform.localToWorldMatrix;
+			Matrix4x4 transformMatrix = otherPortal.transform.localToWorldMatrix * transform.worldToLocalMatrix * frameCamera.transform.localToWorldMatrix;
 			renderCamera.transform.SetPositionAndRotation(transformMatrix.GetPosition(), transformMatrix.rotation);
+			renderCamera.transform.RotateAround(otherPortal.transform.position, otherPortal.transform.forward, 180);
 
 			// up instead of forward bc I'm dumb
-			Vector3 otherPortalNormal = otherPortal.transform.up * (onFrontSide ? -1 : 1);
+			Vector3 otherPortalNormal = otherPortal.transform.up * (onFrontSide ? 1 : -1);
 			Plane clipPlanePlane = new Plane(otherPortalNormal, otherPortal.transform.position - otherPortalNormal * 0.01f);
 			Vector4 clipPlane = new Vector4(clipPlanePlane.normal.x, clipPlanePlane.normal.y, clipPlanePlane.normal.z, clipPlanePlane.distance);
 			Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(renderCamera.worldToCameraMatrix)) * clipPlane;
-			renderCamera.projectionMatrix = camera.CalculateObliqueMatrix(clipPlaneCameraSpace);
+			renderCamera.projectionMatrix = frameCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
 
 			otherPortal.meshRenderer.enabled = false;
-			renderCamera.Render();
+			UniversalRenderPipeline.RenderSingleCamera(context, renderCamera);
 			otherPortal.meshRenderer.enabled = true;
 		}
 	}
